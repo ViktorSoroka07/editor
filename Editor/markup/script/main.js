8c39ae71b76a4dae2e588bc568c5ead5e2817923
Viktor Soroka <Viktor_Soroka@epam.com>
2015-02-13 22:59:50 +0200

    fixed bugs for crossbrowser view of a progect

--- Editor/markup/script/main.js
+++ Editor/markup/script/main.js
@@ -2,90 +2,72 @@
     'use strict';
     var frame,
         frame_content,
-        createFrames;
+        createFrames,
+        frame_window;
     $.fn.editorCustom = function (options) {
         frame = $('<iframe>', {
             id: "editor",
             text: "Your browser does`t support iframes"
         }).appendTo(this);
-        console.log(this);
 
-        frame_content = frame.contents();
+        setTimeout(function () {
+            frame_content = (frame[0].contentDocument) ? frame[0].contentDocument :
+                (frame[0].contentDocument ? frame[0].contentWindow.document : frame[0].document);
+            frame_window = frame[0].contentWindow || frame[0].contentDocument.defaultView;
+            $('<link>', {
+                rel: 'stylesheet',
+                href: 'css/iframe-style.css?t=<?= time(); ?>"'
+            }).appendTo($(frame_content).find('head'));
 
-        $('<link>', {
-            rel: 'stylesheet',
-            href: 'css/iframe-style.css'
-        }).appendTo(frame_content.find('head'));
+            $('<script>', {
+                src: 'script/rangy-core.js'
+            }).appendTo($(frame_content).find('head'));
 
-        $('<div>', {
-            contenteditable: 'true',
-            id: 'edit'
-        }).appendTo(frame_content.find('body'));
+            $('<script>', {
+                src: 'script/rangy-classapplier.js'
+            }).appendTo($(frame_content).find('head'));
 
-        frame.contents().find('#edit').on('keypress', function (event) {
-            var docFragment,
-                newEle,
-                range,
-                sel;
+            $('<div>', {
+                contenteditable: 'true',
+                id: 'edit'
+            }).appendTo($(frame_content).find('body'));
+            //$(frame_content).find('#edit').text('asdasd');
 
-            if (event.which !== 13) {
-                return true;
-            }
-            docFragment = document.createDocumentFragment();
-
-            //add a new line
-            newEle = document.createTextNode('\n');
-            docFragment.appendChild(newEle);
-
-            //add the br, or p, or something else
-            newEle = document.createElement('br');
-            docFragment.appendChild(newEle);
-
-            //make the br replace selection
-            range = document.getElementById('editor').contentWindow.getSelection().getRangeAt(0);
-            range.deleteContents();
-            range.insertNode(docFragment);
-
-            //create a new range
-            range = document.createRange();
-            range.setStartAfter(newEle);
-            range.collapse(true);
-
-            //make the cursor there
-            sel = document.getElementById('editor').contentWindow.getSelection();
-            sel.removeAllRanges();
-            sel.addRange(range);
-
-            return false;
-        });
-
-        createFrames = function () {
-            var params = [
-                    'width=' + screen.width,
-                    'height=' + screen.height,
-                    'top=0',
-                    'left=0',
-                    'fullscreen=yes'
-                ].join(','),
-                newWin = window.open('', 'myEditor', params);
-
-            $(newWin.document.body).ready(function () {
-                var txt = frame_content.find('#edit').html();
-                $(newWin.document.body).html(txt);
+            createFrames = function () {
+                var params = [
+                        'height=' + screen.height,
+                        'width=' + screen.width,
+                        'top=0',
+                        'left=0',
+                        'resizable=yes',
+                        'scrollbars=yes',
+                        'toolbar=yes',
+                        'menubar=yes',
+                        'location=yes'
+                    ].join(','),
+                    newWin = window.open('', 'myEditor', params);
+                $(newWin.document.body).ready(function () {
+                    var txt = $(frame_content).find('#edit').html();
+                    newWin.document.write('<html>' +
+                    '<head>' +
+                        '<title>Preview</title>' +
+                        '<link rel="stylesheet" type="text/css" href="css/preview.css?t=<?= time(); ?>">' +
+                    '</head>' +
+                    '<body>');
+                        $(newWin.document.body).html(txt);
+                        newWin.document.write('</body></html>');
+                });
+                newWin.document.close();
+                if (window.focus) {
+                    newWin.focus();
+                }
+                return false;
+            };
+            $('.preview').on('click', function () {
+                createFrames();
             });
-
-            if (window.focus) {
-                newWin.focus();
-            }
-            return false;
-        };
-
-        $('.preview').on('click', function () {
-            createFrames();
-        });
-        return this;
+            return this;
+        }, 100);
     };
-
     $('#editor-wrap').editorCustom();
-
 }(jQuery));
